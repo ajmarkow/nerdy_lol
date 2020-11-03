1efd3c6648e3e13ee1750236db950ee2f460a763
Forrest Reiland <you@example.com>
2020-11-03 09:20:23 -0800

    add discusion_spec code

---
+++ spec/models/discussion_post_spec.rb
@@ -0,0 +1,2 @@
+require 'rail_helper'
+
--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,1 +1,12 @@
+require 'rails_helper'
 
+describe Discussion do
+  # Association Tests
+    #it { should belong_to :post }
+    #it { should belong_to :user }
+  it { should have_many :discussion_posts}
+  
+  #Validation Tests
+  it { should validate_presence_of :body }
+
+end
