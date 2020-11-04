af2293198ad4a7368c2ab672577fbcadbb8d5917
Alma <a.q.frankenstein@gmail.com>
2020-11-04 15:34:48 -0800

    working Likes button for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :discussion
+  has_many :likes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'discussions#index'
     resources :discussions do
-      resources :posts
+      resources :posts do 
+        resources :likes
+      end
     end
 devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth'}
   devise_scope :user do
