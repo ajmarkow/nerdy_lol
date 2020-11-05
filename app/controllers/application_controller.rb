dc827c8487f12cca9d92dff7796d876a8cd456d7
Alma <a.q.frankenstein@gmail.com>
2020-11-04 16:28:18 -0800

    add flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :alert
   helper_method :is_admin?
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,15 @@ class LikesController < ApplicationController
 
   def create 
     @discussion = Discussion.find(params[:discussion_id])
-    @post.likes.create(user_id: current_user.id)
+    if current_user
+      # @post = Post.find(params[:id])
+      @post.likes.create(user_id: current_user.id)
+      flash[:alert] = "You liked a post"
+      # redirect_to discussion_post_path(@discussion, @post)
+    else 
+      flash[:alert] = "You must be signed in to like a post"
+      #redirect_to discussion_path(@discussion)
+    end
     redirect_to discussion_post_path(@discussion, @post)
   end
 
